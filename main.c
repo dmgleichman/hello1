21910ad55023e415bd12985ec3f6f5a379acb865
Doug Gleichman <Douglas.Gleichman@thinkom.com>
2016-01-11 10:52:17 -0800

    Created a wxDev-C++ project to compile simple C hello world program

---
+++ main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+  	printf("Hello World\n");
+	simple(42);
+	
+    printf("Press enter to continue ...");
+    getchar();	
+    return 0;
+
+}
