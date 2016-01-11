7b0775d88aeda95d4bf1c4d5431846632fca72e0
Doug Gleichman <dmgleichman@yahoo.com>
2016-01-10 17:59:11 -0800

    Added a python script

---
+++ hipy.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+print "Hello World"
+print "This is python running"
+a = 42
+b = 56
+print "A = " + repr(a) + " B = "+ repr(b)
+
+c = a + b
+print "Added together = "+repr(c)
